a717ef24e934d29a1dedeeb707cc794b702b4e13
zyxkad <zyxkad@gmail.com>
2021-10-15 12:14:16 -0600

    node

--- index.js
+++ index.js
@@ -1,85 +1,61 @@
 
 'use strict';
 
-function isInElement($e, $box){
-	//
+function parseTime(s){
+	s = Number.parseInt(s);
+	if(Number.isNaN(s) || s < 0){ s = 0; }
+	return s;
 }
 
-function bindFadeOut($e, event, time=1000){
-	$e.bind(event, function _(e){
-		$e.unbind(event, _);
-		$e.fadeOut(time, function(){
-			moveIn($e.next());
-		});
-	});
-}
-
-function bindSlideOut($e, event, time=1000){
-	$e.bind(event, function _(e){
-		$e.unbind(event, _);
-		$e.slideUp(time, function(){
-			moveIn($e.next());
-		});
-	});
-}
-
-function bindHideOut($e, event, time=1000){
-	$e.bind(event, function _(e){
-		$e.unbind(event, _);
-		$e.hide(time, function(){
-			moveIn($e.next());
-		});
-	});
-}
-
-function moveIn($e){
-	var intype = $e.attr('k-move-in');
-	var time = $e.attr('k-move-in-time');
-	time = (time === undefined) ?1000 :Number.parseFloat(time);
-	if(Number.isNaN(time) || time < 0){ time = 0; }
-	switch(intype){
-		case 'fade':
-			$e.fadeIn(time, function(){
-				bindMoveOut($e);
-			});
-			break;
-		case 'slide':
-			$e.slideDown(time, function(){
-				bindMoveOut($e);
-			});
-			break;
-		default:
-			$e.show(time, function(){
-				bindMoveOut($e);
-			});
+function moveIn($e, call=undefined){
+	var anistyle = $e.is('[k-animation-style]')?JSON.parse('{' + $e.attr('k-animation-style') + '}'):null;
+	var time = $e.is('[k-animation-time]')?parseTime($e.attr('k-animation-time')):1000;
+	var delay = $e.is('[k-animation-delay]')?parseTime($e.attr('k-animation-delay')):0;
+	if(!anistyle){
+		anistyle = {};
+		var anitype = $e.attr('k-anmation-type');
+		switch(anitype){
+			case 'fade':
+				anistyle['opacity'] = 1;
+				break;
+			case 'slide':
+				anistyle['right'] = '0%';
+				break;
+			default:
+				anistyle['opacity'] = 1;
+		}
 	}
+	console.log($e[0], anistyle, time);
+	$e.delay(delay).animate(anistyle, time, call);
 }
 
-function bindMoveOut($e){
-	if(!$e.next().length){
-		console.log('part end');
-		return;
+function moveOut($e, call=undefined, quickly=false){
+	var anistyle = $e.is('[k-animation-style-out]')?JSON.parse('{' + $e.attr('k-animation-style-out') + '}'):null;
+	var time = $e.is('[k-animation-time]')?parseTime($e.attr('k-animation-time')):1000;
+	if(!anistyle){
+		anistyle = {};
+		var anitype = $e.attr('k-anmation-type');
+		switch(anitype){
+			case 'fade':
+				anistyle['opacity'] = 0;
+				break;
+			case 'slide':
+				anistyle['right'] = '-100%';
+				break;
+			default:
+				anistyle['opacity'] = 0;
+		}
 	}
-	var outtype = $e.attr('k-move-out');
-	var time = $e.attr('k-move-out-time');
-	time = (time === undefined) ?1000 :Number.parseFloat(time);
-	if(Number.isNaN(time) || time < 0){ time = 0; }
-	event = 'scroll';
-	switch(outtype){
-		case 'fade':
-			bindFadeOut($e, event, time);
-			break;
-		case 'slide':
-			bindSlideOut($e, event, time);
-			break;
-		default:
-			bindHideOut($e, event, time);
-	}
+	$e.animate(anistyle, quickly?0:time, call);
 }
 
 $(document).ready(function(){
 	const $window = $(window);
 	const $body = $('#body');
+	$body.css({
+		overflow: 'hidden',
+		overflowY: 'scroll'
+	})
 	var parts = $body.children('*[k-part]');
 	parts.each(function(){
 		var $this = $(this);
@@ -88,8 +64,6 @@ $(document).ready(function(){
 			display: 'flex',
 			flexDirection: $this.attr('k-view-column') !== undefined ?'column' :'row',
 			alignItems: 'center',
-			position: 'relative',
-			right: -$window.width() + 'px',
 		})
 		if($this.attr('k-view-height')){
 			var kvh = Number.parseFloat($this.attr('k-view-height'));
@@ -99,22 +73,29 @@ $(document).ready(function(){
 				});
 			}
 		}
+	});
+	$body.find('*[k-view-animation]').each(function(){
+		var $this = $(this);
+		$this.css({
+			position: 'relative'
+		});
 		$window.scroll(function(){
-			if(($this.offset().top - $window.height()) < $window.scrollTop() &&
-			   ($this.offset().top + $this.height()) > $window.scrollTop()){
-				if(!$this[0].goin){
-					$this[0].goin = true;
-					$this.animate({right: '0px'}, 1000, function(){
-						$this[0].goin = undefined;
+			if($this.offset().top < $window.scrollTop() + $window.height() * 2 / 3 &&
+			   $this.offset().top + $this.height() * 2 / 3 > $window.scrollTop()){
+				if(!$this[0].k_animationed){
+					$this[0].k_animationed = 1;
+					moveIn($this, function(){
+						$this[0].k_animationed = null;
 					});
 				}
 			}else{
-				if($this[0].goin !== false){
-					$this[0].goin = false;
-					$this.stop(true).animate({right: -$window.width() + 'px'}, 1000);
+				if($this[0].k_animationed !== 0){
+					$this[0].k_animationed = 0;
+					moveOut($this.stop(true));
 				}
 			}
 		});
+		moveOut($this, undefined, true);
 	});
 	$window.scroll();
 });
